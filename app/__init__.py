d25c3b10a1e19101f8a195278dc9cefa731082d6
JOFLIX <joflixooko@outlook.com>
2019-07-27 23:21:15 +0300

    initialized the application

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+# from newsapi import NewsApiClient
+# from newsapi.newsapi_client import NewsApiClient
+
+bootstrap = Bootstrap(app)
+
+
+def create_app(config_name):
+    bootstrap = Bootstrap()
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+
+    return app
+
